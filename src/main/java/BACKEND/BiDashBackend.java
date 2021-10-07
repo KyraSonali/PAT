8b738a28d6f043a2d5ca71333760d862c36ec9ae
Kyra Balliram <86292244+KyraSonali@users.noreply.github.com>
2021-10-07 16:06:39 +0200

    First commit

---
+++ src/main/java/BACKEND/BiDashBackend.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BACKEND;
+
+/**
+ *
+ * @author Kyra Balliram
+ */
+
+import UI.BiDash;
+public class BiDashBackend {
+    
+    
+    
+    
+    public static void move(){
+        //BiDash.fish.setLocation(fish)
+    }
+    
+}
---
+++ src/main/java/UI/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ *
+ * @author Kyra Balliram
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("GDG");
+    }
+}
